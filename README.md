4fb4ad441eaa2fd8ece098326793375fd53c3ae1
Mick Perkins <mickperkins@gmail.com>
2016-10-19 22:36:35 +0100

    Readme update, tweaks example

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 #Storm load
 
-[![Build Status](https://travis-ci.org/mjbp/storm-component-boilerplate.svg?branch=master)](https://travis-ci.org/mjbp/storm-component-boilerplate)
-[![codecov.io](http://codecov.io/github/mjbp/storm-component-boilerplate/coverage.svg?branch=master)](http://codecov.io/github/mjbp/storm-component-boilerplate?branch=master)
-[![npm version](https://badge.fury.io/js/storm-component-boilerplate.svg)](https://badge.fury.io/js/storm-component-boilerplate)
+[![Build Status](https://travis-ci.org/mjbp/storm-load.svg?branch=master)](https://travis-ci.org/mjbp/storm-load)
+[![codecov.io](http://codecov.io/github/mjbp/storm-load/coverage.svg?branch=master)](http://codecov.io/github/mjbp/storm-load?branch=master)
+[![npm version](https://badge.fury.io/js/storm-load.svg)](https://badge.fury.io/js/storm-load)
 
 Lightweight promise-based script loader
 
--- example/src/app.js
+++ example/src/app.js
@@ -3,18 +3,18 @@ import Load from './libs/storm-load';
 const onDOMContentLoadedTasks = [() => {
     Load(['//d3js.org/d3.v3.min.js'])
         .then(() =>{
-            var data = [4, 8, 15, 16, 23, 42];
+            let data = [4, 8, 15, 16, 23, 42];
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+            let x = d3.scale.linear()
+                .domain([0, d3.max(data)])
+                .range([0, 420]);
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+            d3.select(".chart")
+            .selectAll("div")
+                .data(data)
+            .enter().append("div")
+                .style("width", function(d) { return x(d) + "px"; })
+                .text(function(d) { return d; });
         });
 }];
     
